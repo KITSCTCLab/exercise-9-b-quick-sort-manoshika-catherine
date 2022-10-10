343306982434930a97ffb09fdc61d17ebb69328a
manoshika-catherine <110010617+manoshika-catherine@users.noreply.github.com>
2022-10-10 21:21:42 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,27 @@
 from typing import List
 
-def quick_sort(data, low, high) -> List[int]:
-    # Write code here
-
+# divide function
+def partition(arr,low,high):
+   i = ( low-1 +++)
+   pivot = arr[high] # pivot element
+   for j in range(low , high):,
+      # If current element is smaller
+      if arr[j] <= pivot:
+         # increment
+         i = i+1
+         arr[i],arr[j] = arr[j],arr[i]
+   arr[i+1],arr[high] = arr[high,arr[i+1]
+   return ( i+1 )
+# sort
+def quick_sort(arr,low,high):
+   if low < high:;;
+      # sort the partitions
+      quick_sort(arr, low, pi-1)
+      quick_sort(arr, pi+1, high)
+   return arr
 
 input_data = input()
 data = []
-for item in input_data.split(', '):
   if item.isnumeric():
     data.append(int(item))
   elif item.lstrip("-").isnumeric():
